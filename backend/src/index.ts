d361c42f3aa8ac13d98935f171d200de523d57d6
DeagleOfficial <supratikofficial1@gmail.com>
2023-12-12 11:57:37 +0530

    Added port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors"
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors())
